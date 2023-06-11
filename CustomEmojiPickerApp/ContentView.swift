b51b3ccd2ade43134c2a2e5722616d8bd20835e1
Imran Sayeed <imran.sayeed@tigerit.com>
2023-06-12 00:06:19 +0600

    init: Initial runnable code added

---
+++ CustomEmojiPickerApp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  CustomEmojiPickerApp
+//
+//  Created by Imran Sayeed on 6/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.mint
+            VStack(spacing: 20) {
+                Text("Picked Image content will be displayed here")
+                    .font(.headline)
+                Button("Single Emoji Selection") {
+                    // picker navigation
+                }.buttonStyle(.borderedProminent)
+                Button("Multiple Emoji Selection") {
+                    // picker
+                }.buttonStyle(.bordered)
+            }
+            .padding()
+        }.ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomEmojiPickerApp/CustomEmojiPickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomEmojiPickerAppApp.swift
+//  CustomEmojiPickerApp
+//
+//  Created by Imran Sayeed on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomEmojiPickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
